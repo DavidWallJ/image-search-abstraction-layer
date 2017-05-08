ce73a33410ee789d3864a4293955912de5127c74
DavidWallJ <djkindread@gmail.com>
2017-05-08 19:24:07 +0800

    mongod works locally

--- server.js
+++ server.js
@@ -6,53 +6,80 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const search = require("./models/search");
+const mongodb = require("mongodb");
 
-
+mongoose.Promise = global.Promise;
 const app = express();
-const Bing = require('node-bing-api')({accKey:'508bb882ec9148f9934543cc9aa76265'});
+const Bing = require('node-bing-api')({accKey: '508bb882ec9148f9934543cc9aa76265'});
 
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-// mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost');
+
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/bingapp');
 
 
-var api = '/:searchString(*)';
+const api = '/:searchString(*)';
 
 app.get(api, (req, res, next) => {
-    var { searchString } = req.params;
-    var { offset } = req.query;
+    var skip;
+    var offset;
+    var {searchString} = req.params;
 
-    return res.json({
-        searchString,
-        offset
+
+    // for pagination
+    if(req.query.offset){
+        offset = req.query.offset;
+    } else {
+        offset = 10;
+    }
+
+    if(req.query.skip){
+        skip = req.query.skip;
+    } else {
+        skip = 0;
+    }
+
+    // add search string to DB for recent searches option
+    const data = new search({
+        searchVal: searchString,
+        searchDate: new Date()
+    });
+
+    console.log(data);
+
+    data.save(err => {
+        if(err) {
+            next(err);
+            res.send('Error saving to database');
+        }
+    });
+
+
+    // get bing results
+    Bing.images(searchString, {
+        top: offset,
+        skip: skip
+    }, function (error, res2, body) {
+
+        var bingData = [];
+
+        for(var i=0; i < offset; i++){
+            bingData.push({
+                url: body.value[i].webSearchUrl,
+                snippet: body.value[i].name,
+                thumbnail: body.value[i].thumbnailUrl,
+                context: body.value[i].hostPageDisplayUrl
+            });
+        }
+
+        res.json(bingData);
     });
 });
 
-
-    // const data = new search({
-    //     searchVal: searchString,
-    //     searchDate: Date
-    // });
-    //
-    //     data.save(function(err){
-    //         if(err){
-    //             return res.send('Error saving to database');
-    //         }
-    //     });
-    //
-    //     return res.json(data);
-    // }
-    //
-    // return res.json({
-    //     originalUrl: urlToShorten,
-    //     shortenedUrl: "Invalid URL"
-    // });
-
-
-    // return res.json({url: urlToShorten});
+// return res.json({url: urlToShorten});
 
 
 // Bing.images("Ninja Turtles", {
@@ -67,9 +94,6 @@ app.get(api, (req, res, next) => {
 // });
 
 
-
-
-
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log('Server is up!');
 });
